e56271b8849bf9a71962ffbfc84f9f723e99e92d
phamvanminh7531 <phamvanminh7531@gmail.com>
2025-05-28 14:57:01 +0700

    first commit

---
+++ dbhomes_cms/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-ua7=zkvy9-8z+b6ssqt#bq9f)npzxy4&*&9^3=a7_@7pv#8lj2"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
